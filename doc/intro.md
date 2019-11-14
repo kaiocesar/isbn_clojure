6fa99bf658a575ec7f2ea7b96ed11dcb83a60c5f
Kaio Cesar <tecnico.kaio@gmail.com>
2019-11-14 17:26:27 -0300

    calculo do padrão de ISBN

--- doc/intro.md
+++ doc/intro.md
@@ -10,3 +10,6 @@ Let's take the ISBN-10 3-598-21508-8. We plug it in to the formula, and get:
 
 (3 * 10 + 5 * 9 + 9 * 8 + 8 * 7 + 2 * 6 + 1 * 5 + 5 * 4 + 0 * 3 + 8 * 2 + 8 * 1) mod 11 == 0
 Since the result is 0, this proves that our ISBN is valid.
+
+
+3598215088
--- src/isbn_clojure/isbn_validate.clj
+++ src/isbn_clojure/isbn_validate.clj
@@ -4,5 +4,8 @@
 (defn str-to-int [string]
     (map (fn [n] (Integer/parseInt n)) (re-seq #"\d" string)))
 
+(defn times-to-each [numbers]
+    (map * (range 10 0 -1) (str-to-int numbers)))
+
 (defn validate_isbn [isbn]
-    (str-to-int isbn))
+    (mod (reduce + (times-to-each isbn)) 11))
